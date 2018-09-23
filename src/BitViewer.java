ef768ff24166b570851511d6e76b9c9c676521fd
George Hogg <gwhogg@Georges-MacBook-Pro.local>
2018-09-23 18:23:03 +0100

    Add support for displaying in multiple formats.

--- src/BitViewer.java
+++ src/BitViewer.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.util.Map;
 
 import javax.swing.*;
 import javax.swing.border.Border;
@@ -11,45 +12,113 @@ public class BitViewer {
 
     public class BitsPanel extends JPanel {
 
+        final Map<Integer, Integer> maskMap = Map.of(0, 0x01, 1, 0x02, 2, 0x04, 3, 0x08,
+                4, 0x10, 5, 0x20, 6, 0x40, 7, 0x80);
+        final char[] hexArray = "0123456789ABCDEF".toCharArray();
+
         BitsPanel(byte[] data, DisplayType displayType) {
             setLayout(new GridBagLayout());
             GridBagConstraints gbc = new GridBagConstraints();
+            int numCells;
 
-            int rows = 50;
-            int columns = 75;
+            if (displayType == DisplayType.HEX) {
+                // Dealing with nibbles
+                numCells = data.length * 2;
+            } else {
+                // Dealing with bits
+                numCells = data.length * 8;
+
+            }
+
+            int columns = 50;
+            int rows = numCells / columns;
+            int remainder = numCells % columns;
+
+            int byteNumber = 0;
+            int positionInByte = 0;
+
+            if (remainder != 0) {
+                rows++;
+            }
 
             for (int r = 0; r < rows; r++) {
                 gbc.gridy = r;
                 for (int c = 0; c < columns; c++) {
+                    if (displayType == DisplayType.HEX && c % 2 == 0) {
+                        if (c == remainder) {
+                            break;
+                        }
+                        continue;
+                    }
+
                     gbc.gridx = c;
+                    boolean bit = false;
+                    char nibbleOne = Character.MIN_VALUE;
+                    char nibbleTwo = Character.MIN_VALUE;
+
+                    if (r == rows - 1 && remainder != 0 && c == remainder) {
+                        break;
+                    } else {
+                        byte currentByte = data[byteNumber];
+                        if (displayType != DisplayType.HEX) {
+                            bit = (currentByte & 0xFF & maskMap.get(positionInByte)) != 0;
+                            if (positionInByte == 7) {
+                                byteNumber++;
+                                positionInByte = 0;
+                            } else {
+                                positionInByte++;
+                            }
+                        } else {
+                            nibbleOne = hexArray[currentByte >>> 4];
+                            nibbleTwo = hexArray[currentByte & 0x0F];
+                            byteNumber++;
+                        }
+                    }
 
                     switch (displayType) {
                         case ASCII:
+                            JLabel asciiBitLabel = new JLabel();
+                            asciiBitLabel.setFont(new Font("Courier", Font.PLAIN, 16));
 
+                            if (bit) {
+                                asciiBitLabel.setText("X");
+                            } else {
+                                asciiBitLabel.setText(".");
+                            }
+
+                            add(asciiBitLabel, gbc);
                             break;
                         case BINARY:
+                            JLabel bitLabel = new JLabel();
+                            bitLabel.setFont(new Font("Courier", Font.PLAIN, 16));
+
+                            if (bit) {
+                                bitLabel.setText("1");
+                            } else {
+                                bitLabel.setText("0");
+                            }
+
+                            add(bitLabel, gbc);
+                            break;
+                        case BLOCK:
+                            Color colour = bit ? Color.black : Color.white;
+                            TexturePane texturePane = new TexturePane(colour);
+                            add(texturePane, gbc);
                             break;
                         case HEX:
+                            JLabel nibbleOneLabel = new JLabel();
+                            nibbleOneLabel.setFont(new Font("Courier", Font.PLAIN, 16));
+                            nibbleOneLabel.setText(Character.toString(nibbleOne));
+                            add(nibbleOneLabel, gbc);
+                            JLabel nibbleTwoLabel = new JLabel();
+                            nibbleTwoLabel.setFont(new Font("Courier", Font.PLAIN, 16));
+                            nibbleTwoLabel.setText(Character.toString(nibbleTwo));
+                            gbc.gridx = c + 1;
+                            add(nibbleTwoLabel, gbc);
                             break;
                     }
-                    Color colour;
-                    JLabel bitLabel = new JLabel();
-                    bitLabel.setFont(new Font("Courier", Font.PLAIN, 16));
-
-                    if ((c % 2 == 1 && r % 2 == 1)
-                            || (c % 2 == 0 && r % 2 == 0)) {
-                        bitLabel.setText("0");
-                    } else {
-                        bitLabel.setText("1");
-                    }
-
-                    add(bitLabel, gbc);
-                    //add(new TexturePane(colour), gbc);
                 }
             }
-
-            //setPreferredSize(new Dimension(500, 500));
-            //setBackground(Color.black);
         }
 
         public class TexturePane extends JPanel {
@@ -64,6 +133,7 @@ public class BitViewer {
             }
 
         }
+
     }
 
     public static void main(String[] args) {
@@ -71,8 +141,10 @@ public class BitViewer {
     }
 
     private BitViewer() {
+        byte[] bytes = "George is awesome!".getBytes();
+
         JFrame frame = new JFrame("Bit Viewer");
-        JScrollPane scrPane = new JScrollPane(new BitsPanel(new byte[2], DisplayType.ASCII));
+        JScrollPane scrPane = new JScrollPane(new BitsPanel(bytes, DisplayType.BLOCK));
         frame.getContentPane().add(scrPane);
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
         frame.pack();
@@ -84,6 +156,8 @@ public class BitViewer {
         ButtonPanel.add(btnAsciiZoom);
         JButton btnBinary = new JButton("10");
         ButtonPanel.add(btnBinary);
+        JButton btnBlock = new JButton("BLOCK");
+        ButtonPanel.add(btnBlock);
         JButton btnHex = new JButton("0x");
         ButtonPanel.add(btnHex);
 
--- src/DisplayType.java
+++ src/DisplayType.java
@@ -1,5 +1,6 @@
 public enum DisplayType {
     ASCII,
     BINARY,
+    BLOCK,
     HEX
 }
